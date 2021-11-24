ada03d4da06bb5d102f4ee6722178e746978eb48
Nithiya Soundari K <53947738+Nithiya99@users.noreply.github.com>
2021-11-24 13:46:28 +0800

    backend api initialized

--- server/app.js
+++ server/app.js
@@ -1,5 +1,28 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected!!!");
+  });
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB Connection error: ${err.message}`);
+});
+
+// bring in routes
+app.get("/", (req, res, next) => {
+  res.send("Api running");
+});
+
+// middleware
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
